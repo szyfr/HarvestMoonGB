a8eec85ef01827d17ad3c24771f7e0ae7eb1ae13
Szyfr <jmanz5@hotmail.com>
2022-10-08 19:59:13 -0400

    Finished start

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Harvest Moon GB
+
+A disassembly of Harvest Moon GB. I do have plans to also decompile and examine Harvest Moon GBC, but we'll see if they actually share enough code base to be included here.
+
+Macros are only being used when a peice of code is repeated *SO* much that is makes it easier to read.
+
+## Progress
+NOTE: Numbers are based on a direct comparison between the outputted file and the original. Actual code and documentation is a different story.
+
+[74,517 / 524,288] | ~[0.142%] Similarity as of 2022/10/08.
+
+## Building
+NOTE: A step in the build script is comparing the output ROM to a backup copy of the released game, and that file is not included for obvious reasons. While I *currently* have no plans to switch over to a special build system, I will be working on improving this as I go forward.
+
+### Building on Windows
+Using a cli in the main folder type in the command:
+
+`.\build.bat`
+
+No fancy build system, It just simply creates a directory named /target/debug/%CurrentDate%, copies the src files into it.
+
+It then runs, in this order:
+- rgbasm
+- rgblink
+- rgbfix
+
+Finally it compares the output ROM with an original *backup* located at \ROM\HarvestMoonGB.gb and outputs the result into compared.txt.
+
+### Building on Linux
+TODO:
+
+### Building on Mac
+I'm going to need someone else for this. I don't have any apple products to test on and I don't plan on getting any.
--- src/entry.asm
+++ src/entry.asm
@@ -1,5 +1,7 @@
 
 ;;= Definitions
+include "src/include/hardware.inc"
+
 INCLUDE "src/todo.inc"
 
 
@@ -20,7 +22,223 @@ ds $0150 - @, 0
 
 
 start:
+	nop
+	nop
+	di
 
+	; Sets RAM bank to 0 and ROM bank to 1
+	xor a
+	ld  [rRAMB],a
+	ld  a,1
+	ld  [rROMB0],a
+
+	; Set default stack
+	ld  sp,$DFEF
+
+	; Call FUN_218E
+	ld  hl,$FF80
+	ld  bc,$007F
+	call FUN_218E ; TODO
+
+	; Call FUN_218E
+	ld  hl,$DD00
+	ld  bc,$00FF
+	call FUN_218E ; TODO
+
+	; Call FUN_214E
+	call FUN_214E ; TODO
+
+	xor a
+	ldh [rBGP],a
+	xor a
+	ldh [rOBP0],a
+	xor a
+	ldh [rOBP1],a
+
+	; Call FUN_2426
+	call FUN_2426 ; TODO
+
+	; Call FUN_2166
+	ld  a,2
+	ldh [$8D],a ; TODO
+	call FUN_2166 ; TODO
+
+	xor a
+	ldh [rSCY],a
+	ldh [rSCX],a
+
+	; Set RAM bank to 10
+	ld  a,$0A
+	ld  [rRAMG],a
+
+	; Selects advanced banking mode
+	ld  a,1
+	ld  [rBANKS],a
+
+	; Sets a variable to 0 and calls FUN_232F, jumps if carry
+	xor a
+	ld  [$DD00],a
+	call FUN_232F ; TODO
+	jr  nc,.LAB_01AC
+
+	; Saves hl=af and then calls FUN_208B
+	push hl
+	push af
+
+	ld  l,$A7
+	ld  h,$77
+	ld  a,7
+	call FUN_208B ; TODO
+
+	pop af
+	pop hl
+
+.LAB_01AC:
+	di
+.LAB_01AD:
+	di
+
+	; Clears IE, resets sp, then calls FUN_218E
+	xor a
+	ldh [rIE],a
+	ld  sp,$DFEF
+	ld  hl,$FF80
+	ld  bc,$007F
+	call FUN_218E ; TODO
+
+	; Sets a variable to 2 and calls FUN_2166
+	ld  a,2
+	ldh [$8D],a
+	call FUN_2166 ; TODO
+
+	; Clears SCY+X
+	xor a
+	ldh [rSCY],a
+	ldh [rSCX],a
+
+	; Calls FUN_218E
+	ld  hl,$8000
+	ld  bc,$1FFF
+	call FUN_218E ; TODO
 	
+	; Calls FUN_218E
+	ld  hl,$C000
+	ld  bc,$1CFF
+	call FUN_218E ; TODO
+	
+	; Calls FUN_214E
+	call FUN_214E ; TODO
+
+	; Sets RAM bank to 0
+	xor a
+	ld  [rRAMB],a
+
+	; Sets a variable to $20
+	ld  a,$20
+	ld  [$C0A7],a
+
+	; Calls FUN_2166
+	call FUN_2166 ; TODO
+
+	; Saves hl=af and then calls FUN_208B
+	push hl
+	push af
+
+	ld  l,$26
+	ld  h,$24
+	ld  a,0
+	call FUN_208B ; TODO
+
+	pop af
+	pop hl
+
+.LAB_01F7:
+	di
+
+	; Calls FUN_2166
+	call FUN_2166 ; TODO
+
+	; Saves hl=af and then calls FUN_208B
+	push hl
+	push af
+
+	ld  l,$26
+	ld  h,$24
+	ld  a,0
+	call FUN_208B ; TODO
+
+	pop af
+	pop hl
+
+	; Saves hl=af and then calls FUN_208B
+	push hl
+	push af
+
+	ld  l,$16
+	ld  h,$78
+	ld  a,7
+	call FUN_208B ; TODO
+
+	pop af
+	pop hl
+
+	; Calls FUN_0258
+	xor a
+	ldh [$97],a
+	call FUN_0258 ; TODO
+
+	; Saves hl=af and then calls FUN_208B
+	push hl
+	push af
+
+	ld  l,$26
+	ld  h,$24
+	ld  a,0
+	call FUN_208B ; TODO
+
+	pop af
+	pop hl
+
+	; 
+	xor a
+	ld  [$C0A6],a ; TODO
+	xor a
+	ldh [$8D],a ; TODO
+	xor a
+	ldh [$9A],a ; TODO
+	xor a
+	ld  [$C500],a ; TODO
+	xor a
+	ldh [$97],a ; TODO
+
+	; Saves hl=af and then calls FUN_208B
+	push hl
+	push af
+
+	ld  l,$49
+	ld  h,$79
+	ld  a,7
+	call FUN_208B ; TODO
+
+	pop af
+	pop hl
+
+	; Calls FUN_0061
+	call FUN_0061 ; TODO
+
+	; Calls FUN_217F
+	call FUN_217F ; TODO
+
+	ei
+.LAB_024D:
+	; if [$C0A6] == 0, loop
+	ld  a,[$C0A6] ; TODO
+	or  a
+	or  a
+	jp  z,.LAB_024D
+
+	jp  .LAB_01F7
+
+
 
 SECTION "End", ROMX[$7FFF], BANK[31]
